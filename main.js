f39008ce6f5b59680579ff713a83f4b5306d8339
Marquis Whitley <whitley.marquis1@yahoo.com>
2020-09-26 12:29:53 -0400

    Finished Project

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const icon = document.createElement('i')
 icon.classList.add('fas')
 icon.classList.add('fa-times')
@@ -6,46 +5,58 @@ icon.classList.add('fa-times')
 const form = document.querySelector('#form')
 const task = document.querySelectorAll('.task')
 const input = document.querySelector('#inputfield')
-const container = document.querySelector('.output_container')
+const container = document.querySelector('.output_container');
+const errorMessage = document.querySelector('.error-message')
 
-console.dir(container)
 
-// Create elements
+// Get random Background Color (HexaDecimal)
 
-const createTasks = () => {
+const randomBackgroundColor = () => {
+    const chars = [1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F'];
+    let result = '#';
+    const charactersLength = chars.length
 
-    for(let i = 0; i < task.length; i++){
-        const icon = document.createElement('i');
-        icon.classList.add('fas');
-        icon.classList.add('fa-times');
-
-        task[i].appendChild(icon)
+    for (let i = 0; i < 6; i++) {
+        result += chars[(Math.floor(Math.random() * charactersLength))];
     }
-};
-
-createTasks()
-
-// Close button
-const closeButton = () => {
-    const close = document.querySelectorAll('.fa-times')
-    for(let i = 0; i < close.length; i++){
-        close[i].addEventListener('click', ()=>{
-            task[i].remove();
-        });
-    };
-};
-closeButton()
-
-
+    return result;
+}
 
 // Add Task
 
+const create = () => {
+    if (input.value === '') {
+        errorMessage.innerText = 'Task must have a Value';
+        return;
+    }
+        errorMessage.innerText = '';
 
+        const icon = document.createElement('i');
+        icon.classList.add('fas');
+        icon.classList.add('fa-times');
+        
+        
+        const task = document.createElement('div');
+        task.classList.add('task');
+        task.innerText = input.value;
+        container.appendChild(task);
+        task.appendChild(icon);
+
+        task.addEventListener('click', () => {
+            task.classList.toggle('checked')
+        });
+        icon.addEventListener('click', ()=>{
+            task.remove()
+        });
+};
+
+
+// Post task to the HTML with the submit button
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    
+
+    create()
     console.log(input.value)
     input.value = ''
 })
-
